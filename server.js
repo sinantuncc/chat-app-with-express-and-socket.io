2a96844431ed6e796e21962e6b5c2926963b849c
Sinan Tunç <sinantunc878@gmail.com>
2021-06-15 23:25:04 +0300

    first commit

---
+++ server.js
@@ -0,0 +1,20 @@
+const express = require("express");
+const socket = require("socket.io");
+
+const app = express();
+
+app.use(express.static("public"));
+
+const server = app.listen(3000);
+
+const io = socket(server);
+
+io.on("connection", (socket) => {
+  socket.on("chat", (data) => {
+    io.sockets.emit("chat", data);
+  });
+
+  socket.on("typing", (data) => {
+    socket.broadcast.emit("typing", data);
+  });
+});
